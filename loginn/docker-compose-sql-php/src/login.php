4a7fcc375b5d56048e7ba5085303dc0c23245687
idk <79808407+Idk4041@users.noreply.github.com>
2026-05-11 14:08:48 +0200

    php

---
+++ loginn/docker-compose-sql-php/src/login.php
@@ -0,0 +1,7 @@
+    <?php
+    $name = $_POST['name'];
+    $password = $_POST['password'];
+    echo "Name: " . $name . "<br>";
+    echo "Email: " . $_POST['email'] . "<br>";
+    echo "Password: " . $password . "<br>";
+    ?>
