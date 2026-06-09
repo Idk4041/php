23c763ada4b270bdb93c340016f92494e99ed191
idk <79808407+Idk4041@users.noreply.github.com>
2026-06-09 11:08:04 +0200

    .

--- loginn/login/src/login.php
+++ loginn/login/src/login.php
@@ -34,21 +34,6 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
       <th>Password</th>
       <th>Email</th>
     </tr>
-    <?php
-    $stmt = $conn->prepare("SELECT * FROM user");
-    $stmt->execute();
-    $result = $stmt->get_result();
-    if ($result->num_rows === 0)
-      exit('No rows');
-    while ($row = $result->fetch_assoc()) {
-      echo "<tr>";
-      echo "<td><a href='details.php?id=" . $row['username'] . "'>" . $row['username'] . "</a></td>";
-      echo "<td>" . $row['password'] . "</td>";
-      echo "<td>" . $row['email'] . "</td>";
-      echo "</tr>";
-    }
-    echo "</table>";
-    $stmt->close();
-    ?>
+
 </body>
 </html>
